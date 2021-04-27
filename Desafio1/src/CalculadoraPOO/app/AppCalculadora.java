ba3025f35a428584cde5716066cdfd7d7778488f
Keren Castaño <keren.casta.kco@gmail.com>
2021-04-27 13:03:09 -0500

    Arreglo resta y ciclo

--- Desafio1/src/CalculadoraPOO/app/AppCalculadora.java
+++ Desafio1/src/CalculadoraPOO/app/AppCalculadora.java
@@ -8,6 +8,7 @@ import java.util.Arrays;
 import java.util.List;
 
 public class AppCalculadora {
+    public static final int SEGUIR = -1;
     public static final int SUMAR = 0;
     public static final int RESTAR = 1;
     public static final int MULTIPLICAR = 2;
@@ -22,6 +23,7 @@ public class AppCalculadora {
         int operacionElegida;
         double agregar = 0;
         boolean seguirEjecutando = true;
+        boolean seguir = true;
 
         //CheckBox
         JCheckBox checkbox = new JCheckBox("Redondear");
@@ -30,12 +32,14 @@ public class AppCalculadora {
         //Array de Opciones
         List<Object> operacion = Arrays.asList("Sumar", "Restar", "Multiplicar", "Dividir", checkbox);
 
+        //Array de numeros
+        List<Double> numerosAñadidos = Arrays.asList();
+        numerosAñadidos = new ArrayList<>(numerosAñadidos);
 
         //Repetir indefinidamente
         while(seguirEjecutando) {
             //Petición por pantalla
             operacionElegida = JOptionPane.showOptionDialog(null, "Elija la operación que desea realizar:", "Calculadora POO", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, operacion.toArray(), null);
-
 
             if (operacionElegida == DIVIDIR) {
                 mostrarMensaje("Recuerda que al escoger esta operación no puedes dividir por 0, es un error matemático.");
@@ -44,22 +48,18 @@ public class AppCalculadora {
                 mostrarMensaje("Recuerda que al escoger esta operación no tiene sentido multiplicar por 0, tu resultado será siempre 0");
             }
 
-            //Array de numeros
-            List<Double> numerosAñadidos = Arrays.asList();
-            //Solución del AbstractList?
-            numerosAñadidos = new ArrayList<>(numerosAñadidos);
-
-
             //Ciclo n números
-
             while (operacionElegida != -1) {
                 while (agregar == 0) {
-                    agregar = JOptionPane.showOptionDialog(null, "¿Quieres agregar un nuevo número?", "Número(s) a operar", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
+                    agregar = JOptionPane.showOptionDialog(null, "¿Quieres agregar un nuevo número?", "Números a operar", JOptionPane.YES_NO_OPTION, JOptionPane.QUESTION_MESSAGE, null, null, null);
                     if (agregar == 0) {
                         double numero = Double.parseDouble(JOptionPane.showInputDialog(null, "Ingresa el numero para operar"));
                         numerosAñadidos.add(numero);
                     } else
                         mostrarMensaje("Operación Terminada");
+                    if (agregar == -1){
+                        seguirEjecutando = false;
+                    }
                 }
 
 
@@ -68,47 +68,67 @@ public class AppCalculadora {
                         case SUMAR: {
                             double suma = Calculadora.sumar(numerosAñadidos);
                             double resultado = suma;
-                            if (checkbox.isSelected() == true) {
-                                mostrarMensaje("El resultado es: " + Math.round(resultado));
-                            } else
-                                mostrarMensaje("El resultado es: " + resultado);
+                                while (seguir == true) {
+                                    if (checkbox.isSelected() == true) {
+                                        mostrarMensaje("El resultado es: " + Math.round(resultado));
+                                    } else {
+                                        mostrarMensaje("El resultado es: " + resultado);
+                                    }
+                                    seguir = false;
+                                }
                             break;
                         }
                         case RESTAR: {
                             double resta = Calculadora.restar(numerosAñadidos);
                             double resultado = resta;
-                            if (checkbox.isSelected() == true) {
-                                mostrarMensaje("El resultado es: " + Math.round(resultado));
-                            } else
-                                mostrarMensaje("El resultado es: " + resultado);
+                            while (seguir == true) {
+                                if (checkbox.isSelected() == true) {
+                                    mostrarMensaje("El resultado es: " + Math.round(resultado));
+                                } else {
+                                    mostrarMensaje("El resultado es: " + resultado);
+                                }
+                                seguir = false;
+                            }
                             break;
 
                         }
                         case MULTIPLICAR: {
                             double multiplicacion = Calculadora.multiplicar(numerosAñadidos);
                             double resultado = multiplicacion;
-                            if (checkbox.isSelected() == true) {
-                                mostrarMensaje("El resultado es: " + Math.round(resultado));
-                            } else
-                                mostrarMensaje("El resultado es: " + resultado);
+                            while (seguir == true) {
+                                if (checkbox.isSelected() == true) {
+                                    mostrarMensaje("El resultado es: " + Math.round(resultado));
+                                } else {
+                                    mostrarMensaje("El resultado es: " + resultado);
+                                }
+                                seguir = false;
+                            }
                             break;
                         }
                         case DIVIDIR: {
+
                             double division = Calculadora.dividir(numerosAñadidos);
                             double resultado = division;
                             if (numerosAñadidos.contains(0)) {
                                 mostrarMensaje("Error en los numeros agregados, posiblemente un cero");
                                 break;
                             }
-                            if (checkbox.isSelected() == true) {
-                                mostrarMensaje("El resultado es: " + Math.round(resultado));
-                            } else
-                                mostrarMensaje("El resultado es: " + resultado);
+                            while (seguir == true) {
+                                if (checkbox.isSelected() == true) {
+                                    mostrarMensaje("El resultado es: " + Math.round(resultado));
+                                } else {
+                                    mostrarMensaje("El resultado es: " + resultado);
+                                }
+                                seguir = false;
+                            }
                             break;
 
                         }
-                        default:
-                            mostrarMensaje("No has seleccionado ninguna operación :(");
+                    case SEGUIR:
+                            while (seguir == true) {
+                                mostrarMensaje("No has seleccionado ninguna operación :(");
+                                seguir = false;
+                            }
                             seguirEjecutando = false;
 
                 }
